a1af4733a9bbfd27274d06472c1ef5326f875395
Andrew Moon <ajm80work@gmail.com>
2025-10-21 00:28:24 -0500

    Commit2

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from textblob import TextBlob
 from collections import Counter
 from io import StringIO
 import spacy
-from spacy.cli import download
-download("en_core_web_sm")
 nlp = spacy.load("en_core_web_sm")
 
 # --- App Configuration ---
